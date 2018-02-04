d01a2cdc62bfb1d24c85603d62d11fea531ed9de
Gabriel <1707gabri@gmail.com>
2018-02-04 15:05:00 +0100

    New HomePage

---
+++ Registro/Pages/Menu/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Registro.Pages.Menu
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
